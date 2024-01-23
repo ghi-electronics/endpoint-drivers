ba6847924e5e9b7c407e32bb27f9e22f5c0979b6
Dat Tran <dat.tran@ghielectronics.com>
2024-01-23 10:57:32 -0500

    move folder

--- dotnet/GHIElectronics.Endpoint.Drivers/dopacknuget.bat
+++ dotnet/GHIElectronics.Endpoint.Drivers/dopacknuget.bat
@@ -7,13 +7,17 @@ SET CurrentDir=%CD%
 IF "%DoAssemblySign%" == "true" (
 	pushd "%CurrentDir%"
 
-	IF EXIST "..\..\..\output\%OutputAssemblyName%*.nupkg" (
-		DEL /Q ..\..\..\output\%OutputAssemblyName%*.nupkg
+	IF EXIST "..\..\output" (
+		echo Deleting old folder...
+		rd /q /s ..\..\output	
 	)
+	
+	echo Creating output folder...
+	MKDIR "..\..\output"
 
 	signtool.exe sign /fd sha512 /f "%VsixSignerCertificatePath%" /p "%VsixSignerCertificatePassword%" /t "http://timestamp.digicert.com" /sha1 "%AssemblySignerCertificateSha1%" "%OutputLocation%\%OutputAssemblyName%.dll"
-	nuget pack "%OutputAssemblyName%.nuspec" -Properties Configuration=%BuildMode% -OutputDirectory "..\..\..\output"
-	xcopy /q /y ..\..\..\output\%OutputAssemblyName%*.nupkg %NugetPackerOutputDirectory%
+	nuget pack "%OutputAssemblyName%.nuspec" -Properties Configuration=%BuildMode% -OutputDirectory "..\..\output"
+	xcopy /q /y ..\..\output\%OutputAssemblyName%*.nupkg %NugetPackerOutputDirectory%
 
 	popd
 )
